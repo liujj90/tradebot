53710c6b06adca3a8d4c3b875c38af1964a2eb82
Jiajun Liu <liujj90@gmail.com>
2025-01-13 21:07:13 +0800

    add function that checks last trade time before deciding

--- src/run_decision.py
+++ src/run_decision.py
@@ -12,19 +12,25 @@ percentile_model = PercentileDecision()
 
 class TradeModel:
     fees = 0.25/100 # 0.25% Fees
+    min_usdc_balance = 100
+    min_btc_balance = 0.00001
+    last_buy_ts = datetime.datetime.now() - datetime.timedelta(days = 1) # initialize 
+    last_sell_ts = datetime.datetime.now() - datetime.timedelta(days = 1) 
+    
     def __init__(
         self, 
         btc_bal = 0.0, 
         usdc_bal = 1000, 
         trade_limit = 0.5, 
         # limit_target = 0.002, # 0.2% difference with current market
-        savefilepath = "/home/jj/workspace/experiment.csv"
+        savefilepath = "/home/jj/workspace/experiment.csv",
+        trade_time_threshold = 60 * 60 # 1h
         ):
         self.btc_bal = btc_bal
         self.usdc_bal= usdc_bal
         self.trade_limit = trade_limit
         self.savefilepath = savefilepath 
-
+        self.trade_time_threshold = trade_time_threshold
         
 
     def one_decision(self, override_threshold=None):
@@ -82,26 +88,45 @@ class TradeModel:
         executed = False 
         trade_amt = None
         trade_price = None
-
+        ts = datetime.datetime.now()
+        
         if trade_decision == 'sell': # sell at bid price? 
             trade_amt = self.trade_limit * self.btc_bal
             trade_price = float(current_market['bid'])
-            if self.btc_bal > 0:
-                self.btc_bal -= trade_amt
-                usdc_amt = trade_amt * trade_price 
-                self.usdc_bal += usdc_amt - usdc_amt * self.fees
-                executed = True
-        
+            if self.btc_bal > self.min_btc_balance:
+                if (ts - self.last_sell_ts).seconds > self.trade_time_threshold:
+                    self.btc_bal -= trade_amt
+                    usdc_amt = trade_amt * trade_price 
+                    self.usdc_bal += usdc_amt - usdc_amt * self.fees
+                    executed = True
+                    self.last_sell_ts = ts
+                    description = f"{ts}: sold {trade_amt} btc @ {trade_price} for {usdc_amt} USDC"
+                else:
+                    description = "sell not executed because current time is too close to last sell timestamp"
+
+            else: 
+                description = "sell not executed because min btc balance not reached"
         
         elif trade_decision == 'buy': # buy at ask price?
             trade_amt = self.trade_limit * self.usdc_bal
             trade_price = float(current_market['ask'])
-            if self.usdc_bal > 0:
-                self.usdc_bal -= (trade_amt + trade_amt*self.fees)  
-                self.btc_bal += trade_amt/trade_price
-                executed = True
+            if self.usdc_bal > self.min_usdc_balance:
+                if (ts - self.last_buy_ts).seconds > self.trade_time_threshold:
+                    self.usdc_bal -= (trade_amt + trade_amt*self.fees)  
+                    btc_amt = trade_amt/trade_price
+                    self.btc_bal += btc_amt
+                    executed = True
+                    self.last_buy_ts = ts
+                    description = f"{ts}: buy {btc_amt} @ {trade_price} USDC * {trade_amt}"
+                else:
+                    description = "buy not executed because current time is too close to last buy timestamp"
+            else: 
+                description = "buy not executed because min usdc balance not reached"
 
-        return trade_amt, trade_price, executed
+        else: 
+            description = "decided to hold"
+
+        return trade_amt, trade_price, executed, description
 
     def run(self, override_threshold=None): # only XBTUSDC support right now
         
@@ -109,11 +134,14 @@ class TradeModel:
         current_market = self.get_current_market()
         if to_trade:
             print(f"Trading. Action: {action}")
-            trade_amt, trade_price, executed = self.execute_trade(current_market, action)
+            trade_amt, trade_price, executed, description = self.execute_trade(current_market, action)
             current_market['time_now'] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             current_market['trade_executed'] = executed
             current_market['trade_amt'] = trade_amt
             current_market['trade_price'] = trade_price
+            current_market['usdc_bal'] = self.usdc_bal
+            current_market['btc_bal'] = self.btc_bal
+            current_market['description'] = description
             self.update_balance(current_market)
             print(f"balance updated with data {current_market}") 
         else:
@@ -125,7 +153,7 @@ if __name__ == '__main__':
 
     # run simulation
     while True:
-        model.run(override_threshold=0.8)
+        model.run(override_threshold=1)
         time.sleep(60)
             
 
